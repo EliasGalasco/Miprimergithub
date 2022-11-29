7c0f3f1055251f0cf93b9073657be81325d07365
eliasgalasco <eliasgalasco@gmail.com>
2022-11-29 18:16:22 -0300

    Arreglo js 29/11/2022

--- Js/productos.js
+++ Js/productos.js
@@ -1,17 +1,18 @@
 //TODOS LOS PRODUCTOS
 class productosShine{
-    constructor(id,nombre,descripcion,precio,imagen){
+    constructor(id,nombre,descripcion,precio,imagen,cantidad){
         this.id          = id;
         this.nombre      = nombre;
         this.descripcion = descripcion;
         this.precio      = precio;
         this.imagen      = imagen;
+        this.cantidad    = cantidad;
     }
 };
-const aqtua = new productosShine("1","Aqtua", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 1500, '../imagenes/shop2.jpg' );
-const caffezino = new productosShine("2","Caffezino", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 2500, '../imagenes/shop3.jpg' );
-const ego10 = new productosShine("3","Ego10", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 6500, '../imagenes/shop4.jpg' );
-const egofrutas = new productosShine("4","Egofrutas", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 5500, '../imagenes/shop5.jpg' );
-const egolife = new productosShine("5","Egolife", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 3500, '../imagenes/shop6.jpg' );
-const egoherbal = new productosShine("6","Egoherbal", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 2500, '../imagenes/shop7.jpg' );
-const estop = new productosShine("7","Estop", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 6400, '../imagenes/shop8.jpg' );
+const aqtua = new productosShine("1","Aqtua", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 1500, '../imagenes/shop2.jpg',1 );
+const caffezino = new productosShine("2","Caffezino", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 2500, '../imagenes/shop3.jpg',1 );
+const ego10 = new productosShine("3","Ego10", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 6500, '../imagenes/shop4.jpg',1 );
+const egofrutas = new productosShine("4","Egofrutas", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 5500, '../imagenes/shop5.jpg',1 );
+const egolife = new productosShine("5","Egolife", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 3500, '../imagenes/shop6.jpg',1 );
+const egoherbal = new productosShine("6","Egoherbal", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 2500, '../imagenes/shop7.jpg',1 );
+const estop = new productosShine("7","Estop", "Recomendado para:Presión alta , problemas deinsuficiencia cardíaca, cansanciocrónico, hígado graso y fortalecerel sistema inmune", 6400, '../imagenes/shop8.jpg',1 );
--- Js/shop.js
+++ Js/shop.js
@@ -1,13 +1,21 @@
+/*DOM SELECTOR*/
+const buscadorTienda = document.querySelector(`.buscadorTienda`);
+const btnSearch = document.querySelector(`.btn-search`);
+const carritoContenedor = document.querySelector(`#carritoContenedor`)
+const reiniciarCarro = document.querySelector(`#reiniciarCarro`)
+const precioTotal = document.querySelector(`#precioTotal`)
+const finalizarCompra = document.querySelector(`#finalizarCompra`)
+
+
 
 //ARRAY de productos
 const todosLosProductos = [aqtua, caffezino, ego10, egofrutas, egolife, egoherbal, estop]
-console.log(todosLosProductos);
-//Bucle para cars
-const carrito = []
+/*ARRAY VACIO CARRITO*/
+let carrito = []
 
-let productos = document.querySelector("#productos");
+//Bucle para cars
 for (let prod of todosLosProductos) {
-    const { id, nombre, descripcion, precio, imagen } = prod
+    const { id, nombre, descripcion, precio, imagen, cantidad } = prod
     let cardsProductos = document.createElement("div")
     cardsProductos.className = "col-12 col-md-6 col-lg-6 col-xl-4 cardsBody";
     cardsProductos.innerHTML += `
@@ -16,7 +24,8 @@ for (let prod of todosLosProductos) {
             <div class="card-body">
                 <h5 class="card-title">${nombre}</h5>
                 <p class="card-text">${descripcion}</p>
-                <p class="precioscards">$${precio}</p>
+                <span class="card-text">Cantidad: ${cantidad}</span>
+                <p class="precioscards">Precio: $${precio}</p>
                 <div class="btnTienda">
                     <button id="boton-${id}"  class="btn">Agregar al carrito <i class="fa-solid fa-cart-shopping"></i></i></button>
                 </div>
@@ -28,30 +37,59 @@ for (let prod of todosLosProductos) {
     boton.addEventListener('click', () => {
         agregarProducto(prod)
     })
+
 };
 
-/*FUNCTION PARA BUSCAR POR ID*/
+
+/*FUNCTION btn PARA BUSCADOR */
+
+
+
+/*FUNCION Input BUSCADOR */
+buscadorTienda.addEventListener("input", () => { buscarProducto(buscadorTienda.value, todosLosProductos) })
+
+function buscarProducto(buscador, array) {
+    console.log(buscador)
+    let busqueda = array.filter(
+        (producto) => producto.nombre == producto
+    )
+        console.log(busqueda)
+}
+
+/*REINICIAR CARRITO DE COMPRAS*/
+reiniciarCarro.addEventListener("click", () => {
+    carrito.length = []
+    mostrarCarro(carrito)
+})
+
 /*GUARDAR AL CARRITO*/
 function agregarProducto(prod) {
     Swal.fire({
         position: 'top-end',
         icon: 'success',
-        title: 'El producto fue agregado !!exitosamente!!',
+        title: 'El producto fue agregado con Exito!!',
         showConfirmButton: false,
         timer: 1500
     })
-        carrito.push(prod)
-        console.log(carrito)
-        localStorage.setItem("carrito", JSON.stringify(carrito))
-        mostrarCarro(carrito);
+    carrito.push(prod)
+    mostrarCarro(carrito);
 }
+/*Guardar productos en storage */
+function guardarStorage() {
+    localStorage.setItem("carro", JSON.stringify(carrito))
+}
+document.addEventListener("DOMContentLoaded", () => {
+    carrito = JSON.parse(localStorage.getItem("carro")) || []
+    mostrarCarro()
+})
+
 
 /*Agregar items en el modal*/
-function mostrarCarro(array) {
+const mostrarCarro = () => {
     let modalBody = document.querySelector("#modalBodyId")
     modalBody.innerHTML = ""
-    array.forEach((prod) => {
-        const { id, nombre, precio, imagen } = prod
+    carrito.forEach((prod) => {
+        const { id, nombre, precio, imagen, cantidad } = prod
         modalBody.innerHTML += `
         <div id="productosEnCarrito${id}" class="modal-contenedor">
             <div>
@@ -59,27 +97,79 @@ function mostrarCarro(array) {
             </div>
             <div>
                 <p class="precioscards">Producto: ${nombre} </p>
-                <p class="precioscards"> $${precio} </p>
+                <p class="precioscards">Cantidad: ${cantidad} </p>
+                <p class="precioscards"> Precio: $${precio} </p>
                 <button id="btnEliminar${id}" class="btn btnEliminarCarrito">Eliminar Producto</button>
             </div>
         </div>
         `
     })
     /*Eliminacion */
-    array.forEach((prod, indice) => {
+    carrito.forEach((prod, i) => {
         const { id } = prod
         document.getElementById(`btnEliminar${id}`).addEventListener("click", () => {
             /*Eliminar del DOM */
             let cardProductos = document.querySelector(`#productosEnCarrito${id}`)
             cardProductos.remove()
             /*Eliminar del array */
-            carrito.splice(indice, 1)
+            carrito.splice(i, 1)
             /*Eliminar del Storage */
-            localStorage.setItem("carrito", JSON.stringify(carrito))
-            
+            localStorage.setItem("carro", JSON.stringify(carrito))
+
+
         })
     });
+    if (carrito.length === 0) {
+        modalBody.innerHTML = `
+        <p class="text-center f-bold"> Aun no agregaste ningún producto!</p>
+        `
+    }
+    /*Contador de items del carrito*/
+    carritoContenedor.textContent = carrito.length
+    guardarStorage()
+    /*Precio Total*/
+    precioTotal.textContent = carrito.reduce((suma, producto) => suma + producto.precio, 0)
+
 }
+/*Finalizar Compra SweetAlert */
 
+finalizarCompra.addEventListener(`click`, () => {
+    compraFinalizada()
+})
 
-
+function compraFinalizada() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+        title: 'Estas Seguro que quieres continuar?',
+        text: "Tranquilo/a puede cancelarlo si no lo estas!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, Continuar!',
+        cancelButtonText: 'No, Cancelar!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Excelente!',
+                'Tu orden fue registrada con exito!.',
+                'success'
+            )
+            carrito.length = []
+            localStorage.removeItem("carro")
+        } else if (
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Rechazada!',
+                'Tu orden ha sido rechazada, intentelo de nuevo :)',
+                'error'
+            )
+        }
+    })
+}
